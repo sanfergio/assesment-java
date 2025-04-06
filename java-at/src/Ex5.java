ccec43c0265711ad251b08bbe663ded642a5824f
Giovani Sanfer <143708066+sanfergio@users.noreply.github.com>
2025-04-06 17:08:07 -0300

    Create Ex5.java

---
+++ java-at/src/Ex5.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        System.out.println("Content-Type: text/html");
+        System.out.println();
+
+        // html
+        System.out.println("<html>");
+        System.out.println("<head><title>Saudação CGI</title></head>");
+        System.out.println("<body>");
+        System.out.println("<h1>Olá, Terráqueos!</h1>");
+        System.out.println("</body>");
+        System.out.println("</html>");
+    }
+}
